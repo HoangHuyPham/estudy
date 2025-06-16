c0e2a946dc0defa7a512a0ed0568616bb0110289
HoangHuyPham <hoanghuy7284022@gmail.com>
2025-06-16 20:10:55 +0700

    update course controller

--- be/src/main/java/vn/edu/hcmuaf/be/controller/CourseController.java
+++ be/src/main/java/vn/edu/hcmuaf/be/controller/CourseController.java
@@ -1,24 +1,22 @@
 package vn.edu.hcmuaf.be.controller;
 
-import java.util.UUID;
+import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
-
 import vn.edu.hcmuaf.be.dto.ApiPaginationResponse;
 import vn.edu.hcmuaf.be.dto.PaginationQuery;
+import vn.edu.hcmuaf.be.dto.course.CourseDTO;
 import vn.edu.hcmuaf.be.entity.Course;
 import vn.edu.hcmuaf.be.repository.CourseRepository;
 
@@ -27,19 +25,23 @@ import vn.edu.hcmuaf.be.repository.CourseRepository;
 public class CourseController {
     @Autowired
     private CourseRepository repoCourse;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ApiPaginationResponse<?>> getAll(PaginationQuery query) {
         Pageable pageable = PageRequest.of(query.getPage(), query.getLimit());
         Page<Course> pageResult = repoCourse.findAll(pageable);
+        List<CourseDTO> result = pageResult.getContent().stream().map(e->modelMapper.map(e, CourseDTO.class)).toList();
 
-        return ResponseEntity.ok(
+        return new ResponseEntity<>(
                 ApiPaginationResponse.builder()
                         .limit(query.getLimit())
                         .page(query.getPage())
                         .total((int) pageResult.getTotalElements())
-                        .data(pageResult.getContent())
+                        .data(result)
                         .message("success")
-                        .build());
+                        .build(),
+                HttpStatus.OK);
     }
 }
