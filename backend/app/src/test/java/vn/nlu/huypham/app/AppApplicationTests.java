2670779256db8bcc9a0c1e64cb436026c406633d
Huy Phạm <hoanghuy7284022@gmail.com>
2026-03-03 17:39:45 +0700

    Initial commit

---
+++ backend/app/src/test/java/vn/nlu/huypham/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.nlu.huypham.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
